6043a3bbb8c3e4509b0d279d82af1d4347b5d270
Martin Grignard <mar.grignard@gmail.com>
2025-08-22 20:27:11 +0200

    refactor: :recycle: Make child reference private

--- src/player/player.gd
+++ src/player/player.gd
@@ -5,12 +5,12 @@ extends CharacterBody2D
 @export var jump_velocity: float
 @export var jump_cooldown: float
 
-@onready var jump_cooldown_timer: Timer = get_node("JumpCooldown")
+@onready var _jump_cooldown_timer: Timer = get_node("JumpCooldown")
 
 func _physics_process(delta: float) -> void:
 	var acceleration = gravity_acceleration
 	self.velocity.y += delta * acceleration
-	if Input.is_action_just_pressed("Jump") and jump_cooldown_timer.is_stopped():
+	if Input.is_action_just_pressed("Jump") and _jump_cooldown_timer.is_stopped():
 		self.velocity.y -= jump_velocity
-		jump_cooldown_timer.start(jump_cooldown)
+		_jump_cooldown_timer.start(jump_cooldown)
 	self.move_and_slide()
